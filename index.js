e290b025b1c76ba25e1fd0251427028f81c8b308
Keith Prifte <kprift@gmail.com>
2016-04-22 12:17:05 -0400

    adds handlebars and views

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var express = require("express");
+// ::start:: dependencies
+var express  = require("express");
+var mongoose = require("./db/connection.js");
+var hbs      = require("express-handlebars")
+var app      = express();
+//  ::end:: dependencies
+var Events   = mongoose.model("Events");
 
-var app     = express();
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:       ".hbs",
+  partialsDir:   "views/",
+  layoutsDir:    "views/",
+  defaultLayout: "layout-main"
+}));
 
 app.get('/', function(req, res){
-  res.send("Hello World");
+  Events.find().then(function(events){
+    res.render("events-index", {
+      events
+    });
+  });
 });
 
 app.listen(3002, function(){
