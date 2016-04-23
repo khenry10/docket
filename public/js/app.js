975efc744a39ed0897d9d924cfc4bde56d37fef1
Keith Prifte <kprift@gmail.com>
2016-04-23 10:09:41 -0400

    adds js calendar table and first two rows

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,63 @@
 var date = new Date()
 
 var month = date.getMonth() //returns 0-11 (0 is Janauary)
-month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
+var month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
 
 var d = date.getDate() // returns day of the month.  Example: returns "22" on April 22nd 2016
 
 var weekday = date.getDay() // returns 0-6 (0 is sunday)
 
-days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+days_of_week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 var year = date.getFullYear()  // returns 2016
 
-var days = new Date(year, month+1, 0).getDate() //returns 30, which is the number of days in April.  For some reason January = 1 here
+var number_of_days = new Date(year, month+1, 0).getDate() //returns 30, which is the number of days in April.  For some reason January = 1 here
 
 var first_day_of_month = new Date(year, month, 1).getDay() //returns 5 (which is Friday) for April
 
-// module.exports = days[weekday] + " " + month[m] + " " + d + ", " + year;
-module.exports = days_of_week[first_day_of_month];
+var first_date = month_name[month] + " " + 1 + " " + year; // April 1 2016
+
+var tmp = new Date(first_date).toDateString()
+var first_day = tmp.substr(0, 3) // returns Fri
+
+var day_no = days_of_week.indexOf(first_day)
+
+console.log(day_no)
+
+
+// passing in day_no which is the value (0 -6) of the first day of the month and number_of_days which is the number of days in each month
+function make_calendar(day_no, number_of_days){
+  console.log("make_calendar invoked")
+  var table = document.createElement("table");
+  console.log(table)
+  var tr    = document.createElement("tr");
+  // row for Day names
+  for(var i = 0; i < 7 ; i++){
+    var th = document.createElement("th")
+    th.innerHTML = days_of_week[i]
+    tr.appendChild(th)
+  }
+  table.appendChild(tr)
+
+  // create 2nd row, which is the first row of dates
+  tr = document.createElement("tr");
+  for(var i = 0; i < 7; i++){
+    if(i === day_no){
+      break;
+    }
+      var td = document.createElement("td");
+      td.innerHTML = "";
+      tr.appendChild(td)
+  }
+    var count = 1;
+    for(; i < 7; i++){
+      var td = document.createElement("td");
+      td.innerHTML = count;
+      count++;
+      tr.appendChild(td);
+    }
+    table.appendChild(tr)
+    console.log(table)
+}
+
+make_calendar(first_day_of_month, number_of_days)
