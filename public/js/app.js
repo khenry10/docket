51d3568d3dce22865564cd9d0dc7f7203031089f
Keith Prifte <kprift@gmail.com>
2016-10-02 17:41:40 -0400

    adds protocol planning

--- public/js/app.js
+++ public/js/app.js
@@ -44,6 +44,10 @@
     "$scope",
     listController
   ])
+  .controller("protocolController", [
+    "$scope",
+    protocolController
+  ])
 
   function router($stateProvider, $locationProvider){
     $locationProvider.html5Mode(true);
@@ -66,6 +70,12 @@
       controller: "listController",
       controllerAs: "listsVM"
     })
+    .state("protocol", {
+      url: "/protocol-planning",
+      templateUrl: "/assets/html/protocol-planning.html",
+      controller: "protocolController",
+      controllerAs: "protocolVM"
+    })
     .state("show", {
       url: "/:name",
       templateUrl: "/assets/html/show.html",
@@ -73,6 +83,7 @@
       controllerAs: "showVM"
     })
   }
+
   function Lists($resource){
     console.log("Lists factory envoked")
     var Lists = $resource("/expenses", {}, {
@@ -100,6 +111,43 @@
     return Events
   };
 
+  function protocolController(){
+    var vm = this;
+
+    vm.protocols = ["Antagonist", "Lupron Trigger", "Clomid IUI"]
+
+    vm.enter = function (protocol){
+      console.log("protocol = " + protocol)
+      var protocol = protocol
+      vm.datesEntered = true
+      var numberOfBCDays = vm.birthControl;
+      var firstDayOfBCDate = vm.firstDayOfBC;
+      var firstDayofBCMonth = firstDayOfBCDate.getMonth();
+      var firstDayofBCYear = firstDayOfBCDate.getFullYear();
+      var bcDay = firstDayOfBCDate.getDate();
+
+      vm.dayAfterBC = bcDay + numberOfBCDays;
+      vm.dateAfterBC = new Date(firstDayofBCYear,firstDayofBCMonth, vm.dayAfterBC);
+
+      vm.stimDays = []
+      for(var i = 3; i < 15; i++){
+          vm.stimDays.push(new Date(firstDayofBCYear, firstDayofBCMonth, vm.dayAfterBC + i))
+      };
+      vm.triggerDay = vm.stimDays[11];
+      var triggerDate = vm.triggerDay.getDate();
+      var triggerMonth = vm.triggerDay.getMonth();
+      var triggerYear = vm.triggerDay.getFullYear();
+
+      if(protocol === "Lupron Trigger"){
+        vm.lupronTrigger = new Date(triggerYear, triggerMonth, triggerDate+1);
+      }
+
+      vm.eggRetrieval = new Date(triggerYear, triggerMonth, triggerDate+2);
+      vm.embryoTransfer = new Date(triggerYear, triggerMonth, triggerDate+7);
+      vm.pregnancyTest = new Date(triggerYear, triggerMonth, triggerDate+20);
+    }
+
+  };
 
   function listController(Lists){
     var finances = [];
