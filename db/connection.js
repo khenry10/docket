595fe6653a7ec263e18e33395282339f891b9788
Prifte <keith.prifte@washpost.com>
2016-11-14 19:50:59 -0500

    4th heroku push

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/docket");
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGOLAB_URI);
+}else{
+  mongoose.connect("mongodb://localhost/docket");
+}
 
 var EventSchema = new mongoose.Schema(
   {
