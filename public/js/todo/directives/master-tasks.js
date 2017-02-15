fe99024a4fbec45815aad367b492456b7294476b
Prifte <keith.prifte@washpost.com>
2017-02-15 05:59:24 -0500

    adds create new master tasks from left menu functionality

--- public/js/todo/directives/master-tasks.js
+++ public/js/todo/directives/master-tasks.js
@@ -12,19 +12,37 @@
     return {
       templateUrl: "/assets/html/todo/directives/master-tasks.html",
       link: function(scope){
-        scope.newMaster = ""
+
         console.log("You in da MASTA Todo")
+
         scope.lists = []
         Todo.all.$promise.then(function(){
           Todo.all.forEach(function(todo){
               console.log(todo)
-              scope.lists.push({name: todo.list_name, master_tasks: todo.master_tasks})
-            
+              scope.lists.push({name: todo.list_name, master_tasks: todo.master_tasks, lists: todo.lists})
+
           })
         })
 
-        scope.addNewMaster = function (){
-          console.log(scope.newMaster)
+
+        scope.addNewMaster = function (master, list){
+          console.log(master)
+          console.log(list)
+          var today = new Date()
+          var saveMe = {
+            name: master,
+            task_completed: false
+          }
+          list.master_tasks.push({name: master, created_on: today})
+          list.lists.forEach(function(recurring){
+            console.log(recurring)
+            recurring.tasks.push(saveMe)
+          })
+          list.list_name = list.name
+          console.log(list)
+          Todo.update({list_name: list.list_name}, {todo: list}, function(task){
+            console.log(task)
+          })
         }
 
       }
